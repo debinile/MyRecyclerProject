158b0991084b8627599c4d3312f27ff0c47c0f1c
debinile <debinile@yahoo.com>
2021-07-06 08:15:52 -0700

    recycler

---
+++ app/src/main/java/com/ajoritsedebi/myrecyclerproject/UserDetailsActivity.kt
@@ -0,0 +1,25 @@
+package com.ajoritsedebi.myrecyclerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tinyit.recyclerclick.databinding.ActivityUserDetailsBinding
+
+class UserDetailsActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityUserDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val surName: String = intent.getStringExtra("surName").toString()
+        val lastName: String = intent.getStringExtra("lastName").toString()
+        val img: Int = intent.getIntExtra("img",9)
+        binding.txtDetailsSurName.text = surName
+        binding.txtDetailsLastName.text = lastName
+        binding.imgDetailsPhoto.setImageResource(img)
+
+    }
+}
